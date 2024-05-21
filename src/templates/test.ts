58df71a102bc8c4a805f94d0ef3b5c3a14dd7977
rai <mj82xx@gmail.com>
2024-05-21 16:18:50 +0900

    chore: bump version

--- src/templates/test.ts
+++ src/templates/test.ts
@@ -8,7 +8,7 @@ import {
   getMetadata,
   getProblemDir,
   METADATA_FILE_NAME,
-} from "jsr:@r4ai/atcoder-cli@0.2.3"
+} from "jsr:@r4ai/atcoder-cli@0.2.4"
 
 const testsDir = import.meta.dirname!
 const problemDir = getProblemDir(testsDir)
